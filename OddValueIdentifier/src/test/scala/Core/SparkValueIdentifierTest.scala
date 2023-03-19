93d5dfc42dd30e98daa80b15849a3b8f24e52004
Thales Vinicius <obthales@gmail.com>
2023-03-19 14:44:27 +0100

    Unit test working.

--- OddValueIdentifier/src/test/scala/Core/SparkValueIdentifierTest.scala
+++ OddValueIdentifier/src/test/scala/Core/SparkValueIdentifierTest.scala
@@ -14,10 +14,14 @@ class SparkValueIdentifierTest extends AnyFlatSpec with Matchers {
   import sc.implicits._
 
   private val testDf = Seq((1, 2), (1, 3), (1, 3), (2, 4), (2, 4), (2, 4)).toDF("key", "value")
-  private val expectedDf = Seq((1, 2), (2, 4)).toDF("key", "value")
+  private val expectedDf = Seq((1, 2), (2, 4)).toDF("key", "value").collect()
 
   "SparkValueIdentifier" should "return the odd values for each key" in {
-    val result = SparkValueIdentifier.IdentifyOddValues(testDf)
-    result shouldBe expectedDf
+    val result = SparkValueIdentifier.IdentifyOddValues(testDf).collect()
+
+    result(0)(0) shouldBe expectedDf(0)(0)
+    result(0)(1) shouldBe expectedDf(0)(1)
+    result(1)(0) shouldBe expectedDf(1)(0)
+    result(1)(1) shouldBe expectedDf(1)(1)
   }
 }
